7c9d0e5faea5e87703134fe96ad94f3c18cbd5b9
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-30 10:52:45 +0600

    digitools basic resource setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      
+<h2 className="text-3xl font-bold text-purple-500 text-center uppercase py-5">Hello DigiTools </h2>    </div>
+  );
+};
+
+export default App;
